1a08803f7019401796899afd9216b9036507cc8c
eduardnol <eduardo.nolla@outlook.com>
2022-02-15 10:45:18 +0100

    Cors policy

--- src/main/java/com/example/database_user/Model/Persona/PersonaController.java
+++ src/main/java/com/example/database_user/Model/Persona/PersonaController.java
@@ -13,21 +13,21 @@ import java.util.List;
 public class PersonaController {
     private final PersonaService personaService;
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/allpeople")
     public ResponseEntity<List<Persona>> fetchAllPeople() {
 
         return ResponseEntity.status(HttpStatus.OK).body(personaService.fetchAllPeople());
     }
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/search/{name}")
     public ResponseEntity<List<Persona>> fetchPeopleByName(@PathVariable("name") String name) {
 
         return personaService.fetchPeopleByName(name);
     }
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/daterange/{initial}/{final}")
     public ResponseEntity<List<Persona>> fetchDateRangePeople(@PathVariable("initial") String initialDate, @PathVariable("final") String finalDate) {
 
@@ -35,7 +35,7 @@ public class PersonaController {
 
     }
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping(value = "/insertnew/")
     public ResponseEntity<String> insertNewUser(@RequestBody Persona persona) {
 
@@ -43,14 +43,14 @@ public class PersonaController {
 
     }
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @DeleteMapping(value = "/deletebyid")
     public ResponseEntity<String> deleteUser(@RequestParam(name = "id") String identificador) {
 
         return personaService.deletePerson(identificador);
     }
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @PutMapping(value = "/update/")
     public ResponseEntity<String> updateExisting(@RequestBody Persona persona) {
 
