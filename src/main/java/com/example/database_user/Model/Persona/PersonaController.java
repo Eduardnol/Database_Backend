73a35f804682f1a7b55bb155e55df8dca03229b4
eduardnol <eduardo.nolla@outlook.com>
2022-02-16 13:02:39 +0100

    Format

--- src/main/java/com/example/database_user/Model/Persona/PersonaController.java
+++ src/main/java/com/example/database_user/Model/Persona/PersonaController.java
@@ -13,17 +13,20 @@ import java.util.List;
 public class PersonaController {
     private final PersonaService personaService;
 
+
     @GetMapping("/allpeople")
     public ResponseEntity<List<Persona>> fetchAllPeople() {
 
         return ResponseEntity.status(HttpStatus.OK).body(personaService.fetchAllPeople());
     }
 
+
     @GetMapping(value = "/search/{name}")
     public ResponseEntity<List<Persona>> fetchPeopleByName(@PathVariable("name") String name) {
 
         return personaService.fetchPeopleByName(name);
     }
+
 
     @GetMapping(value = "/daterange/{initial}/{final}")
     public ResponseEntity<List<Persona>> fetchDateRangePeople(@PathVariable("initial") String initialDate, @PathVariable("final") String finalDate) {
@@ -32,6 +35,7 @@ public class PersonaController {
 
     }
 
+
     @PostMapping(value = "/insertnew/")
     public ResponseEntity<String> insertNewUser(@RequestBody Persona persona) {
 
@@ -39,11 +43,13 @@ public class PersonaController {
 
     }
 
+
     @DeleteMapping(value = "/deletebyid")
     public ResponseEntity<String> deleteUser(@RequestParam(name = "id") String identificador) {
 
         return personaService.deletePerson(identificador);
     }
+
 
     @PutMapping(value = "/update/")
     public ResponseEntity<String> updateExisting(@RequestBody Persona persona) {
