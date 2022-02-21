33057b88aa3b7cf04c309c0f07efccbac16c4cc3
Eduardo <eduardnol@gmail.com>
2022-02-21 16:27:17 +0100

    Better mappings at file

--- src/main/java/com/example/database_user/Model/Common/FileStorageController.java
+++ src/main/java/com/example/database_user/Model/Common/FileStorageController.java
@@ -3,9 +3,7 @@ package com.example.database_user.Model.Common;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +15,18 @@ public class FileStorageController {
     private final FileStorageService fileStorageService;
 
 
-    @GetMapping("/allpeople")
-    public ResponseEntity<List<FileStorage>> fetchAllPeople() {
+    @GetMapping("/allfiles")
+    public ResponseEntity<List<FileStorage>> fetchAllPeople(@RequestParam(name = "id") String identificador) {
 
         return ResponseEntity.status(HttpStatus.OK).body(fileStorageService.fetchAllFiles());
+    }
+
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<String> updateExisting(@RequestBody FileStorage file, @RequestParam(name = "id") String identificador) {
+
+        //return fileStorageService.addFile(file);
+
     }
 
 }
