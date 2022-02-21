7a26a0d3f33b69f58bac3c9f0b05609c4f54ca83
Eduardo <eduardnol@gmail.com>
2022-02-21 13:38:33 +0100

    File Storage creation

---
+++ src/main/java/com/example/database_user/Model/Common/FileStorage.java
@@ -0,0 +1,10 @@
+package com.example.database_user.Model.Common;
+
+import lombok.Data;
+
+@Data
+public class FileStorage {
+    private String originalName;
+    private String newName;
+    private String location;
+}
