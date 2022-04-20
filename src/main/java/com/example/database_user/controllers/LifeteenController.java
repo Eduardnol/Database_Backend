951c91a543a5aaf5348e1780acbf85489b2b8d91
eduardnol <eduardo.nolla@outlook.com>
2022-04-20 11:31:13 +0200

    Added api endpoints

--- src/main/java/com/example/database_user/controllers/LifeteenController.java
+++ src/main/java/com/example/database_user/controllers/LifeteenController.java
@@ -5,9 +5,9 @@ import com.example.database_user.dtos.Lifeteen;
 import com.example.database_user.services.LifeteenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,9 +16,32 @@ import java.util.List;
 public class LifeteenController {
 	private final LifeteenService lifeteenService;
 
+
+	@GetMapping("/all")
 	public ResponseEntity<List<Lifeteen>> fetchAllLifeteen() {
 
 		return lifeteenService.fetchAllLifeteen();
+	}
+
+
+	@PostMapping("/insert")
+	public ResponseEntity<String> insertNewLifeteen(@RequestBody @Valid Lifeteen lifeteen) {
+
+		return lifeteenService.insertNewLifeteen(lifeteen);
+	}
+
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteLifeteen(@PathVariable String id) {
+
+		return lifeteenService.deleteById(id);
+	}
+
+
+	@PutMapping("/update")
+	public ResponseEntity<String> updateLifeteen(@RequestBody @Valid Lifeteen lifeteen) {
+
+		return lifeteenService.updateLifeteen(lifeteen);
 	}
 
 }
