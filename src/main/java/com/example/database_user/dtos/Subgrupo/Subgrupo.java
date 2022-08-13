3fe69305ab7e97bc626edfdcfcf5d3c4af9898da
eduardnol <eduardo.nolla@outlook.com>
2022-08-13 13:46:43 +0200

    Subgroup constructor added

--- src/main/java/com/example/database_user/dtos/Subgrupo/Subgrupo.java
+++ src/main/java/com/example/database_user/dtos/Subgrupo/Subgrupo.java
@@ -1,11 +1,13 @@
 package com.example.database_user.dtos.Subgrupo;
 
 import com.example.database_user.dtos.Persona.SimplePersona;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Subgrupo {
     private String titulo;
     private List<SimplePersona> responsables;
