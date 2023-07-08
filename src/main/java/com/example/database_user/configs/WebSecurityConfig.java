8cd0afdb6adf0f93de9b1a192f2e9db868c5ac1c
Eduardo <eduardnol@gmail.com>
2023-07-08 17:54:35 +0200

    Java now inserts information into meilisearch correctly

--- src/main/java/com/example/database_user/configs/WebSecurityConfig.java
+++ src/main/java/com/example/database_user/configs/WebSecurityConfig.java
@@ -1,36 +1,35 @@
-package com.example.database_user.configs;
-
-import java.util.List;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-//TODO revisar que significa todo esto
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-
-    http.cors().and().csrf().disable();
-  }
-
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*"));
-    configuration.setAllowedMethods(List.of("*"));
-    configuration.setAllowedHeaders(List.of("*"));
-    configuration.setAllowCredentials(false);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-
-}
+//package com.example.database_user.configs;
+//
+//import java.util.List;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+////TODO revisar que significa todo esto
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//
+//    http.cors().and().csrf().disable();
+//  }
+//
+//
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(List.of("*"));
+//    configuration.setAllowedMethods(List.of("*"));
+//    configuration.setAllowedHeaders(List.of("*"));
+//    configuration.setAllowCredentials(false);
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
+//
+//}
