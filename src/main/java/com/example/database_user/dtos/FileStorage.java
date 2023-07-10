430a1856617f75644ff73c3404c91e905ceb43a3
Eduardo <eduardnol@gmail.com>
2023-07-10 10:40:01 +0200

    Solved person insertion bug

--- src/main/java/com/example/database_user/dtos/FileStorage.java
+++ src/main/java/com/example/database_user/dtos/FileStorage.java
@@ -3,8 +3,10 @@ package com.example.database_user.dtos;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FileStorage {
 
