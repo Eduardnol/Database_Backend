42fd17229b92337f36ac840938d3f7295bdd9078
Eduardo <eduardnol@gmail.com>
2024-01-31 18:20:21 +0100

    Updated security configurations

--- src/main/java/com/example/database_user/configs/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/database_user/configs/security/SpringSecurityConfiguration.java
@@ -1,17 +1,16 @@
 package com.example.database_user.configs.security;
 
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -31,21 +30,21 @@ public class SpringSecurityConfiguration {
   }
 
 
-  // We can also create multiple users and give role based access to them
+/*  // We can also create multiple users and give role based access to them
   @Bean
   public UserDetailsService multipleUsers() {
-    /*
+    *//*
      * Note: do not use withDefaultPasswordEncoder() method in production since its
      * not recommended and its not safe. This is used only for development and learning purpose ,
      * use your own password generating methods.
-     */
+   *//*
     UserBuilder user = User.withDefaultPasswordEncoder();
     InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
     manager.createUser(user.username("faiz").password("user_password").roles("USER").build());
     manager.createUser(user.username("user").password("user").roles("USER").build());
 
     return manager;
-  }
+  }*/
 
   @Bean
   public SecurityFilterChain configureMultipleUserRoles(HttpSecurity httpSecurity) throws Exception {
