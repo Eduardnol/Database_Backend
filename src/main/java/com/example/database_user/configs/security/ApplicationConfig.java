24276fce968f77fccb1ef73536395fe68fa03a90
Eduardo <eduardnol@gmail.com>
2024-02-01 17:29:07 +0100

    Filter established

--- src/main/java/com/example/database_user/configs/security/ApplicationConfig.java
+++ src/main/java/com/example/database_user/configs/security/ApplicationConfig.java
@@ -5,8 +5,14 @@ import com.example.database_user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,5 +25,24 @@ public class ApplicationConfig {
   public UserDetailsService userDetailsService() {
     return username -> userRepository.findByEmail(username).map(userMapper::toDTO)
         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
--- src/main/java/com/example/database_user/configs/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/database_user/configs/security/SpringSecurityConfiguration.java
@@ -1,57 +1,53 @@
 package com.example.database_user.configs.security;
 
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfiguration {
+
+  private final JwtAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
+
   @Bean
   public UserDetailsService singleUser() {
     InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
     manager.createUser(
-        User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build());
+        User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN")
+            .build());
     return manager;
   }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-        .formLogin(withDefaults()).httpBasic(withDefaults());
+    httpSecurity
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .requestMatchers("")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy((SessionCreationPolicy.STATELESS))
+        .and()
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return httpSecurity.build();
   }
 
-
-/*  // We can also create multiple users and give role based access to them
-  @Bean
-  public UserDetailsService multipleUsers() {
-    *//*
-     * Note: do not use withDefaultPasswordEncoder() method in production since its
-     * not recommended and its not safe. This is used only for development and learning purpose ,
-     * use your own password generating methods.
-   *//*
-    UserBuilder user = User.withDefaultPasswordEncoder();
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    manager.createUser(user.username("faiz").password("user_password").roles("USER").build());
-    manager.createUser(user.username("user").password("user").roles("USER").build());
-
-    return manager;
-  }*/
-
-  @Bean
-  public SecurityFilterChain configureMultipleUserRoles(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf((csrf) -> csrf.disable()).securityMatcher("/bookstore/api/v1/admin**")
-        .authorizeHttpRequests(auth -> auth.anyRequest().hasRole("ADMIN")).httpBasic(withDefaults());
-
-    return httpSecurity.build();
-
-  }
 }
