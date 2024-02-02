635185ca57cf3395b07a2178e821c7c34c694e72
Eduardo Nolla <eduardo.nolla@ithinkupc.com>
2024-02-02 12:16:03 +0100

    Upgrated to new security standards

--- src/main/java/com/example/database_user/configs/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/database_user/configs/security/SpringSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,18 +23,14 @@ public class SpringSecurityConfiguration {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-        .csrf()
-        .disable()
-        .authorizeRequests()
-        .requestMatchers("/api/v1/auth/**", "/v3/api-docs/**", "/swagger-ui/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy((SessionCreationPolicy.STATELESS))
-        .and()
+    httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/api/v1/auth/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return httpSecurity.build();
