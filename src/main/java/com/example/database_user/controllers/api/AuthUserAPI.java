3c1612d9bf88fb132bd16e50f210260e2f3055ce
Eduardo Nolla <eduardo.nolla@ithinkupc.com>
2024-02-02 11:23:07 +0100

    Added documentation

--- src/main/java/com/example/database_user/controllers/api/AuthUserAPI.java
+++ src/main/java/com/example/database_user/controllers/api/AuthUserAPI.java
@@ -3,19 +3,24 @@ package com.example.database_user.controllers.api;
 import com.example.database_user.configs.security.auth_messages.AuthenticationRequest;
 import com.example.database_user.configs.security.auth_messages.AuthenticationResponse;
 import com.example.database_user.configs.security.auth_messages.RegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "AuthUser", description = "Register and authenticate users")
 @RestController
 @RequestMapping("/api/v1/auth")
 public interface AuthUserAPI {
 
+  @Operation(summary = "Register a new user")
   @PostMapping("/register")
   ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest);
 
+  @Operation(summary = "Authenticate a user")
   @PostMapping("/authenticate")
   ResponseEntity<AuthenticationResponse> authenticate(
       @RequestBody AuthenticationRequest authenticateRequest);
