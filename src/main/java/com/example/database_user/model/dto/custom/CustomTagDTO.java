b1db1f6ed5baad027abebd709dd2c686334dc01f
eduardonolla <eduardo.nolla@outlook.com>
2024-03-05 19:28:11 +0100

    Custom tags UUID generation test

--- src/main/java/com/example/database_user/model/dto/custom/CustomTagDTO.java
+++ src/main/java/com/example/database_user/model/dto/custom/CustomTagDTO.java
@@ -1,25 +1,21 @@
 package com.example.database_user.model.dto.custom;
 
+import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 
 /**
  * CustomTagDTO fields for the person class, you can personalize with selected, name and tagname
  */
+@Builder
 @Data
 @AllArgsConstructor
 public class CustomTagDTO {
-
-  private String id;
+  @Builder.Default
+  private String id = UUID.randomUUID().toString();
   private String typeOfData;
   private String data;
   private String tagName;
-
-
-  public CustomTagDTO() {
-    typeOfData = "";
-    data = "";
-    tagName = "";
-  }
 }
--- src/main/java/com/example/database_user/repositories/entity/CustomTagEntity.java
+++ src/main/java/com/example/database_user/repositories/entity/CustomTagEntity.java
@@ -1,15 +1,19 @@
 package com.example.database_user.repositories.entity;
 
+import java.util.UUID;
+import javax.annotation.processing.Generated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+
 @Builder
 @Data
 @AllArgsConstructor
 public class CustomTagEntity {
   @Id
-  private String id;
+  @Builder.Default
+  private String id = UUID.randomUUID().toString();
   private String typeOfData;
   private String data;
   private String tagName;
