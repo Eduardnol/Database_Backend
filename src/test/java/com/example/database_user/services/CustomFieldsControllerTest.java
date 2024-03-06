579dae51a6d95abbb2ce5356fe7f4d5521e2a419
Eduardo Nolla <eduardo.nolla@ithinkupc.com>
2024-03-06 08:57:14 +0100

    Migrated test to use endpoint
    Tests now use the endpoint rather than the function for testing

--- src/test/java/com/example/database_user/services/CustomFieldsControllerTest.java
+++ src/test/java/com/example/database_user/services/CustomFieldsControllerTest.java
@@ -1,23 +1,37 @@
 package com.example.database_user.services;
 
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.example.database_user.controllers.impl.CustomFieldsController;
 import com.example.database_user.model.dto.custom.CustomFieldsDTO;
-import com.example.database_user.model.dto.Reponses.MainResponse;
 import com.example.database_user.model.dto.custom.CustomTagDTO;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CustomFieldsControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @InjectMocks
   private CustomFieldsController customFieldsController;
@@ -31,7 +45,7 @@ public class CustomFieldsControllerTest {
   }
 
   @Test
-  public void testCreateCustomTag() {
+  public void testCreateCustomTag() throws Exception {
     CustomTagDTO customTagDTO = CustomTagDTO.builder()
         .id("tagId")
         .tagName("tagName")
@@ -46,13 +60,11 @@ public class CustomFieldsControllerTest {
         .customTagDTOS(customTagDTOList)
         .build();
 
-    MainResponse expectedResponse = new MainResponse("Custom tag created", HttpStatus.CREATED,
-        customFieldsDTO);
     when(customFieldsService.createCustomTag(customFieldsDTO)).thenReturn(customFieldsDTO);
 
-    ResponseEntity<MainResponse> responseEntity = customFieldsController.createCustomTag(
-        customFieldsDTO);
-
-    assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    mockMvc.perform(post("/api/v1/custom-tag/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(customFieldsDTO)))
+        .andExpect(status().isCreated());
   }
 }
