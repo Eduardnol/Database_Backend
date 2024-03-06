ece5b391110f865b2e979ce45d65ee79df48bc7a
eduardonolla <eduardo.nolla@outlook.com>
2024-03-06 18:40:18 +0100

    Added more tests for custom fields

--- src/test/java/com/example/database_user/configuration/BaseTest.java
+++ src/test/java/com/example/database_user/configuration/BaseTest.java
@@ -1,17 +1,21 @@
 package com.example.database_user.configuration;
 
 import com.example.database_user.clock.ClockProvider;
+import com.example.database_user.configs.security.JWTService;
+import com.example.database_user.model.dto.AuthUserDTO;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
@@ -30,13 +34,23 @@ public class BaseTest {
 
   @Autowired
   private ClockProvider clockProvider;
+
+  static JWTService jwtService = new JWTService();
+
+  private static String token;
 /*
   @ClassRule
   public static MongoDBContainer mongoDBContainer = TestMongoDBContainer.getInstance();*/
 
   @BeforeAll
   static void arrancaContenedor() {
-   // mongoDBContainer.start();
+    // mongoDBContainer.start();
+    AuthUserDTO userDetailsDTO = AuthUserDTO.builder()
+        .email("admin")
+        .password("admin")
+        .build();
+    token = jwtService.generateToken(userDetailsDTO);
+
   }
 
   @AfterEach
@@ -45,7 +59,8 @@ public class BaseTest {
   }
 
   protected void setCurrentTime(final LocalDateTime date) {
-    this.clockProvider.setClock(Clock.fixed(date.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault()));
+    this.clockProvider.setClock(
+        Clock.fixed(date.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault()));
   }
 
 
--- src/test/java/com/example/database_user/services/CustomFieldsControllerTest.java
+++ src/test/java/com/example/database_user/services/CustomFieldsControllerTest.java
@@ -2,17 +2,18 @@ package com.example.database_user.services;
 
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import com.example.database_user.controllers.impl.CustomFieldsController;
+
 import com.example.database_user.model.dto.custom.CustomFieldsDTO;
 import com.example.database_user.model.dto.custom.CustomTagDTO;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,14 +28,18 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 public class CustomFieldsControllerTest {
 
+  private static final String BASE = "http://localhost:8080/api/v1/people";
+  private static final String CUSTOM_TAG = BASE + "/custom-tag";
+  private static final String CREATE_CUSTOM_TAG = CUSTOM_TAG + "/create";
+  private static final String DELETE_BY_ID = CUSTOM_TAG + "/delete/{tagId}";
+  private static final String GET_ALL = CUSTOM_TAG + "/get-all";
+
+
   @Autowired
   private MockMvc mockMvc;
 
-  @Autowired
+  @Mock
   private ObjectMapper objectMapper;
-
-  @InjectMocks
-  private CustomFieldsController customFieldsController;
 
   @Mock
   private CustomFieldsService customFieldsService;
@@ -62,9 +67,23 @@ public class CustomFieldsControllerTest {
 
     when(customFieldsService.createCustomTag(customFieldsDTO)).thenReturn(customFieldsDTO);
 
-    mockMvc.perform(post("/api/v1/custom-tag/create")
+    mockMvc.perform(post(CREATE_CUSTOM_TAG)
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(customFieldsDTO)))
         .andExpect(status().isCreated());
   }
+
+  @Test
+  public void deleteCustomTagById() throws Exception {
+    String tagId = "tagId";
+    mockMvc.perform(delete(DELETE_BY_ID, tagId))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void testGetAllCustomTags() throws Exception {
+    mockMvc.perform(get(GET_ALL))
+        .andExpect(status().isOk());
+  }
+
 }
