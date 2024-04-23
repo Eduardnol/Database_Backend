a0315be720d623053019a0bdb09c176d449adc26
Eduardo Nolla <eduardo.nolla@ithinkupc.com>
2024-04-23 12:10:26 +0200

    Added builder

--- src/main/java/com/example/database_user/repositories/entity/PersonaSacramentsEntity.java
+++ src/main/java/com/example/database_user/repositories/entity/PersonaSacramentsEntity.java
@@ -1,8 +1,12 @@
 package com.example.database_user.repositories.entity;
 
 import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Data
+@Builder
 public class PersonaSacramentsEntity {
 
   // @Pattern(regexp = "^(bautismo|confirmación|eucaristía|penitencia|unción|orden|matrimonio|)$")
