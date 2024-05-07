a4d47c724a037ac5a65341c2f764d8baf2df2869
Eduardo Nolla <eduardo.nolla@ithinkupc.com>
2024-05-07 13:31:40 +0200

    Changed the application.yaml to join ServiceConnection

--- src/test/java/com/example/database_user/configuration/BaseTest.java
+++ src/test/java/com/example/database_user/configuration/BaseTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,10 +16,10 @@ import org.testcontainers.utility.MountableFile;
 
 @SpringBootTest
 @Testcontainers
-@ActiveProfiles("test")
 public class BaseTest {
 
   private static final String IMAGE_VERSION = "mongo:4.4.6";
+
   @Container
   @ServiceConnection
   static MongoDBContainer mongoContainer = new MongoDBContainer(
@@ -28,6 +27,7 @@ public class BaseTest {
       .withCopyFileToContainer(
           MountableFile.forClasspathResource("init-script.js"),
           "/docker-entrypoint-initdb.d/init-mongo.js");
+
   protected String token;
   @Autowired
   private JWTService jwtService;
