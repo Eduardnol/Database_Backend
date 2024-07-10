b5a957e6ee1b8a6543ec731536ee9ccd4967d3fe
Eduardo Nolla <eduardo.nolla@ithinkupc.com>
2024-07-10 15:48:58 +0200

    Se ha hecho la clase BaseTest abstracta

--- src/test/java/com/example/database_user/configuration/BaseTest.java
+++ src/test/java/com/example/database_user/configuration/BaseTest.java
@@ -16,13 +16,14 @@ import org.testcontainers.utility.MountableFile;
 
 @SpringBootTest
 @Testcontainers
-public class BaseTest {
+public abstract class BaseTest {
 
   private static final String IMAGE_VERSION = "mongo:4.4.6";
   @Container
   @ServiceConnection
   static MongoDBContainer mongoContainer = new MongoDBContainer(
       DockerImageName.parse(IMAGE_VERSION))
+      .withReuse(true)
       .withCopyFileToContainer(
           MountableFile.forClasspathResource("init-script.js", 777),
           "./data/mongo-init:/docker-entrypoint-initdb.d/init-mongo.js");
